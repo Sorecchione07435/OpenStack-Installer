096c28012a9ca47a6258aa36fe7e2f0bf440f6c2
Sorecchione07435 <Sorecchione07@gmail.com>
2026-07-27 08:46:16 +0200

    update

--- deploystack/cmds/deploy/main.py
+++ deploystack/cmds/deploy/main.py
@@ -112,6 +112,8 @@ def deploy(parser, args) -> None:
 
         neutron_driver = args.neutron_driver if args.neutron_driver in ("ovs","ovn") else "ovs"
         manila_backend = args.manila_backend if args.manila_backend in ("generic", "lvm") else "lvm"
+
+        manila_share_protocols = args.manila_share_protocols if args.manila_share_protocols in ("cifs", "nfs") else "nfs"
         
         lvm_size = args.lvm_image_size_in_gb if cinder_flag == "yes" else 0
 
@@ -125,6 +127,7 @@ def deploy(parser, args) -> None:
             lvm_image_size_in_gb=lvm_size,
             neutron_driver=neutron_driver,
             manila_backend=manila_backend,
+            manila_share_protocols=manila_share_protocols
             os_release=openstack_release
         )
 
