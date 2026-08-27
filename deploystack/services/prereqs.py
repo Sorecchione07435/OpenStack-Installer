3376414ae3d74e2724e66a3693f2c0b9c42cba4d
Sorecchione07435 <Sorecchione07@gmail.com>
2026-08-27 18:44:05 +0200

    update

--- deploystack/services/prereqs.py
+++ deploystack/services/prereqs.py
@@ -222,7 +222,9 @@ def install_pkgs(config):
         if not is_package_installed("python3-pip"):
             if not apt_install(["python3-pip"], "Installing system python pip package...") : return False
 
-        if not run_command(["pip", "install", "python-openstackclient", "--break-system-packages"], "Installing OpenStack Client from system pip...") : return False
+        if not run_command(["pip", "install", "python-openstackclient==9.0.0", "--break-system-packages"], "Installing OpenStack Client from system pip...") : return False
+
+        print()
         
     if devices:
         prereqs_pkgs.append("lvm2")
