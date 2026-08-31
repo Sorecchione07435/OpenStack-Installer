6d3c827ae49622320dd555f7028557cff90f6f7c
Sorecchione07435 <Sorecchione07@gmail.com>
2026-08-31 16:30:36 +0200

    update

--- deploystack_loopback/cli/__init__.py
+++ deploystack_loopback/cli/__init__.py
@@ -1,13 +1,13 @@
 import argparse
 
-from ...cmds.attach import build_attach_subparser
-from ...cmds.check import build_check_parser
-from ...cmds.detach import build_detach_parser
-from ...cmds.filter import build_filter_parser
-from ...cmds.reconcile import build_reconcile_parser
-from ...cmds.start import build_start_parser
-from ...cmds.status import build_status_parser
-from ...cmds.stop import build_stop_parser
+from ..cmds.attach import build_attach_subparser
+from ..cmds.check import build_check_parser
+from ..cmds.detach import build_detach_parser
+from ..cmds.filter import build_filter_parser
+from ..cmds.reconcile import build_reconcile_parser
+from ..cmds.start import build_start_parser
+from ..cmds.status import build_status_parser
+from ..cmds.stop import build_stop_parser
 
 def build_parser() -> argparse.ArgumentParser:
 
@@ -16,18 +16,18 @@ def build_parser() -> argparse.ArgumentParser:
         description="Manage Deploystack LVM loopback storage"
     )
 
-    subparsers = parser.add_subparser(
+    subparsers = parser.add_subparsers(
         dest="command",
         required=True
     )
 
-    build_attach_subparser(parser)
-    build_check_parser(parser)
-    build_detach_parser(parser)
-    build_filter_parser(parser)
-    build_reconcile_parser(parser)
-    build_start_parser(parser)
-    build_status_parser(parser)
-    build_stop_parser(parser)
+    build_attach_subparser(subparsers)
+    build_check_parser(subparsers)
+    build_detach_parser(subparsers)
+    build_filter_parser(subparsers)
+    build_reconcile_parser(subparsers)
+    build_start_parser(subparsers)
+    build_status_parser(subparsers)
+    build_stop_parser(subparsers)
 
     return parser
